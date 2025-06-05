57b7e67dee603571b97cefc3b20c890f151d4ed3
matjaz sercer <matjazsercer1@gmail.com>
2025-06-05 23:05:43 +0200

    Initial FireCMS setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { FireCMSCloudApp } from "@firecms/cloud";
+import appConfig from "./index";
+
+function App() {
+    return <FireCMSCloudApp
+        projectId={"wintoget"}
+        appConfig={appConfig}
+    />;
+}
+
+export default App
